bbe555ab5f3bb8cabba892a3639041766743f12b
Alucard Stormbringer <supersayan1000@gmail.com>
2026-02-03 15:32:51 +0530

    UI/UX Polish 8/n
    Implement animated navbar

--- lib/widgets/glass_nav_bar.dart
+++ lib/widgets/glass_nav_bar.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:liquid_glass_renderer/liquid_glass_renderer.dart';
 
 /// A floating glass navigation bar with iOS 18-style design
-class FloatingGlassNavBar extends StatelessWidget {
+class FloatingGlassNavBar extends StatefulWidget {
   final int selectedIndex;
   final ValueChanged<int> onDestinationSelected;
   final List<NavigationDestination> destinations;
@@ -13,6 +13,101 @@ class FloatingGlassNavBar extends StatelessWidget {
     required this.onDestinationSelected,
     required this.destinations,
   });
+
+  @override
+  State<FloatingGlassNavBar> createState() => _FloatingGlassNavBarState();
+}
+
+class _FloatingGlassNavBarState extends State<FloatingGlassNavBar>
+    with TickerProviderStateMixin {
+  late List<AnimationController> _iconControllers;
+  late List<Animation<double>> _iconScaleAnimations;
+  late List<Animation<double>> _iconRotationAnimations;
+  late AnimationController _bubbleAnimationController;
+  late Animation<double> _bubbleSquashAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeAnimations();
+  }
+
+  void _initializeAnimations() {
+    _iconControllers = List.generate(
+      widget.destinations.length,
+      (index) => AnimationController(
+        vsync: this,
+        duration: const Duration(milliseconds: 400),
+      ),
+    );
+
+    _iconScaleAnimations = _iconControllers.map((controller) {
+      return Tween<double>(
+        begin: 1.0,
+        end: 1.2,
+      ).animate(CurvedAnimation(parent: controller, curve: Curves.elasticOut));
+    }).toList();
+
+    _iconRotationAnimations = _iconControllers.map((controller) {
+      return Tween<double>(
+        begin: 0.0,
+        end: 0.1,
+      ).animate(CurvedAnimation(parent: controller, curve: Curves.easeInOut));
+    }).toList();
+
+    // Bubble squash and stretch animation
+    _bubbleAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 350),
+    );
+
+    _bubbleSquashAnimation = TweenSequence<double>([
+      TweenSequenceItem(
+        tween: Tween<double>(
+          begin: 1.0,
+          end: 0.96,
+        ).chain(CurveTween(curve: Curves.easeOut)),
+        weight: 35,
+      ),
+      TweenSequenceItem(
+        tween: Tween<double>(
+          begin: 0.96,
+          end: 1.04,
+        ).chain(CurveTween(curve: Curves.easeInOutCubic)),
+        weight: 35,
+      ),
+      TweenSequenceItem(
+        tween: Tween<double>(
+          begin: 1.04,
+          end: 1.0,
+        ).chain(CurveTween(curve: Curves.easeOut)),
+        weight: 30,
+      ),
+    ]).animate(_bubbleAnimationController);
+  }
+
+  @override
+  void didUpdateWidget(FloatingGlassNavBar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.selectedIndex != widget.selectedIndex) {
+      // Animate the bubble squash and stretch
+      _bubbleAnimationController.forward(from: 0.0);
+
+      // Animate the newly selected icon
+      _iconControllers[widget.selectedIndex].forward().then((_) {
+        _iconControllers[widget.selectedIndex].reverse();
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    for (var controller in _iconControllers) {
+      controller.dispose();
+    }
+    _bubbleAnimationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,19 +126,74 @@ class FloatingGlassNavBar extends StatelessWidget {
           child: Container(
             height: 74,
             padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: List.generate(
-                destinations.length,
-                (index) => _buildNavItem(
-                  context,
-                  destinations[index],
-                  index,
-                  selectedIndex == index,
-                ),
-              ),
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                return Stack(
+                  children: [
+                    // Animated selection bubble
+                    _buildSelectionBubble(context, constraints.maxWidth),
+                    // Navigation items
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: List.generate(
+                        widget.destinations.length,
+                        (index) => _buildNavItem(
+                          context,
+                          widget.destinations[index],
+                          index,
+                          widget.selectedIndex == index,
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSelectionBubble(BuildContext context, double containerWidth) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final itemWidth = containerWidth / widget.destinations.length;
+
+    return AnimatedPositioned(
+      duration: const Duration(milliseconds: 350),
+      curve: Curves.easeInOutCubic,
+      left: itemWidth * widget.selectedIndex,
+      top: 0,
+      bottom: 0,
+      width: itemWidth,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 4),
+        child: AnimatedBuilder(
+          animation: _bubbleSquashAnimation,
+          builder: (context, child) {
+            final squashValue = _bubbleSquashAnimation.value;
+
+            return Transform.scale(
+              scale: squashValue,
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      colorScheme.primary.withValues(alpha: 0.15),
+                      colorScheme.primary.withValues(alpha: 0.08),
+                    ],
+                  ),
+                  borderRadius: BorderRadius.circular(16),
+                  border: Border.all(
+                    color: colorScheme.primary.withValues(alpha: 0.2),
+                    width: 1,
+                  ),
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
@@ -60,79 +210,67 @@ class FloatingGlassNavBar extends StatelessWidget {
         ? colorScheme.primary
         : colorScheme.onSurface.withValues(alpha: 0.6);
 
-    final content = InkWell(
-      onTap: () => onDestinationSelected(index),
-      borderRadius: BorderRadius.circular(16),
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 200),
-        curve: Curves.easeOutCubic,
-        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 2),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            AnimatedSwitcher(
-              duration: const Duration(milliseconds: 200),
-              transitionBuilder: (child, animation) {
-                return ScaleTransition(scale: animation, child: child);
-              },
-              child: IconTheme(
-                key: ValueKey(isSelected),
-                data: IconThemeData(color: color, size: 24),
-                child: isSelected
-                    ? (destination.selectedIcon ?? destination.icon)
-                    : destination.icon,
-              ),
-            ),
-            const SizedBox(height: 4),
-            AnimatedDefaultTextStyle(
-              duration: const Duration(milliseconds: 200),
-              curve: Curves.easeOutCubic,
-              style: TextStyle(
-                fontSize: 10,
-                fontWeight: isSelected ? FontWeight.w600 : FontWeight.normal,
-                color: color,
-                height: 1.2,
-              ),
-              child: Text(
-                destination.label,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-
     return Expanded(
-      child: AnimatedScale(
-        scale: isSelected ? 1.0 : 0.95,
-        duration: const Duration(milliseconds: 200),
-        curve: Curves.easeOutCubic,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 4),
-          child: isSelected
-              ? Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [
-                        colorScheme.primary.withValues(alpha: 0.15),
-                        colorScheme.primary.withValues(alpha: 0.08),
-                      ],
+      child: InkWell(
+        onTap: () => widget.onDestinationSelected(index),
+        borderRadius: BorderRadius.circular(16),
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.easeOutCubic,
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 2),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedBuilder(
+                animation: _iconControllers[index],
+                builder: (context, child) {
+                  return Transform.scale(
+                    scale: _iconScaleAnimations[index].value,
+                    child: Transform.rotate(
+                      angle: _iconRotationAnimations[index].value,
+                      child: AnimatedSwitcher(
+                        duration: const Duration(milliseconds: 250),
+                        transitionBuilder: (child, animation) {
+                          return ScaleTransition(
+                            scale: animation,
+                            child: FadeTransition(
+                              opacity: animation,
+                              child: child,
+                            ),
+                          );
+                        },
+                        child: IconTheme(
+                          key: ValueKey('$index-$isSelected'),
+                          data: IconThemeData(color: color, size: 24),
+                          child: isSelected
+                              ? (destination.selectedIcon ?? destination.icon)
+                              : destination.icon,
+                        ),
+                      ),
                     ),
-                    borderRadius: BorderRadius.circular(16),
-                    border: Border.all(
-                      color: colorScheme.primary.withValues(alpha: 0.2),
-                      width: 1,
-                    ),
-                  ),
-                  child: content,
-                )
-              : content,
+                  );
+                },
+              ),
+              const SizedBox(height: 4),
+              AnimatedDefaultTextStyle(
+                duration: const Duration(milliseconds: 200),
+                curve: Curves.easeOutCubic,
+                style: TextStyle(
+                  fontSize: 10,
+                  fontWeight: isSelected ? FontWeight.w600 : FontWeight.normal,
+                  color: color,
+                  height: 1.2,
+                ),
+                child: Text(
+                  destination.label,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
